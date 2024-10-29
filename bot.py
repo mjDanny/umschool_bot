b3a220c6d7bd9409f54aa952bb939a104f3e7f59
mjDanny <bakhvalovfresh2014@gmail.com>
2024-10-29 17:30:42 +0300

    students_db

--- bot.py
+++ bot.py
@@ -11,16 +11,23 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
 
 TOKEN = TOKEN
 
 dp = Dispatcher()
 
 
-async def add_to_database(telegram_id, username):
+class Registration(StatesGroup):
+    waiting_for_first_name = State()
+    waiting_for_last_name = State()
+
+
+async def add_to_database(telegram_id, first_name, last_name):
     async with aiosqlite.connect("students.db") as db:
         await db.execute(
-            "CREATE TABLE IF NOT EXISTS users (telegram_id BIGINT , username TEXT, date TEXT)"
+            "CREATE TABLE IF NOT EXISTS users (telegram_id BIGINT, first_name TEXT, last_name TEXT, date TEXT)"
         )
         cursor = await db.execute(
             "SELECT * FROM users WHERE telegram_id = ?", (telegram_id,)
@@ -32,23 +39,47 @@ async def add_to_database(telegram_id, username):
     date = f"{datetime.date.today()}"
     async with aiosqlite.connect("students.db") as db:
         await db.execute(
-            "INSERT INTO users (telegram_id, username, date) VALUES (?,?,?)",
-            (telegram_id, username, date),
+            "INSERT INTO users (telegram_id, first_name, last_name, date) VALUES (?,?,?,?)",
+            (telegram_id, first_name, last_name, date),
         )
         await db.commit()
 
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    await message.answer(f"Hello!")
+    await message.answer(
+        f"Привет! Зарегистрируйся или войди в свой аккаунт с помощью команды /register."
+    )
+
+
+@dp.message(Command("register"))
+async def cmd_register(message: Message, state: FSMContext):
+    await message.answer("Введите имя:")
+    await state.set_state(Registration.waiting_for_first_name)
+
+
+@dp.message(F.text, Registration.waiting_for_first_name)
+async def process_first_name(message: Message, state: FSMContext):
+    first_name = message.text
+    await state.update_data(first_name=first_name)
+    await message.answer("Введите фамилию:")
+    await state.set_state(Registration.waiting_for_last_name)
+
+
+@dp.message(F.text, Registration.waiting_for_last_name)
+async def process_last_name(message: Message, state: FSMContext):
+    last_name = message.text
+    user_data = await state.get_data()
+    first_name = user_data.get("first_name")
     telegram_id = message.from_user.id
-    username = message.from_user.username
-    await add_to_database(telegram_id, username)
 
+    # Вызов функции для добавления данных в базу данных
+    await add_to_database(telegram_id, first_name, last_name)
 
-@dp.message(F.text, Command('reg'))
-async def test(message: Message) -> None:
-    await message.answer("регайся")
+    await message.answer(
+        f"Регистрация завершена. Ваши данные: {first_name} {last_name}"
+    )
+    await state.clear()
 
 
 async def main() -> None:
